a85d282a53b8606c76f90e9b11b89b08ca8d88a1
Hilman Fikry <hilman12fikry@gmail.com>
2025-06-29 20:02:22 +0700

    fix: list styled to be normal

---
+++ src/components/Food.jsx
@@ -0,0 +1,14 @@
+function Food() {
+  return (
+    <>
+      <h1>My favorite food</h1>
+      <ul>
+        <li>Sate padang</li>
+        <li>Batagor</li>
+        <li>Burger</li>
+      </ul>
+    </>
+  );
+}
+
+export default Food;
